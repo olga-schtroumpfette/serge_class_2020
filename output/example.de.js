cc62158dae0245683dd5de7866e6f71821d6c1fa
Olga <51927486+olga-schtroumpfette@users.noreply.github.com>
2020-10-29 01:36:02 +0300

    Update example.de.js

--- output/example.de.js
+++ output/example.de.js
@@ -1,4 +1,4 @@
 export default {
-    'title': 'Hello, world!', // Page title
+    'title': 'Hallo Welt!', // Page title
     'message': "This is the newest version of the old message from Github", // Text that appears below the title
 };
