ba118dfb99c9865a7ffc43d540944672f1f08031
Olga <51927486+olga-schtroumpfette@users.noreply.github.com>
2020-10-29 01:24:38 +0300

    Update example.js

--- en/example.js
+++ en/example.js
@@ -1,4 +1,5 @@
 export default {
     'title': 'Hello, world!', // Page title
+    'new line': 'Added in GitHub'
     'message': "Let's try the continuous localization approach!", // Text that appears below the title
 };
