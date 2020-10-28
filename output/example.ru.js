561d430972ee9c3dff0c748d65e62096f7852902
L10N Robot <l10n@mycompany.com>
2020-10-28 22:30:29 +0000

    Automatic commit of updated project files

--- output/example.ru.js
+++ output/example.ru.js
@@ -1,4 +1,4 @@
 export default {
     'title': 'Привет, мир!', // Page title
-    'message': "Let\'s try the continuous localization approach!", // Text that appears below the title
+    'message': "This is the newest version of the old message from Github", // Text that appears below the title
 };
